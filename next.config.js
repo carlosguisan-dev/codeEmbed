20232a53d1a825d82d661a45e83b864eb76953ea
Carlos Guillermo Sánchez Cabezas <carlosguisan@gmail.com>
2025-12-28 20:55:45 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -7,26 +7,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      }
-    ],
+    remotePatterns: [],
   },
   devIndicators: {
     buildActivity: true,

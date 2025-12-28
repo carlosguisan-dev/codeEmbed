3574ebc7a19ac1a3caf0eeafab328bab2d9c8584
Carlos Guillermo Sánchez Cabezas <carlosguisan@gmail.com>
2025-12-28 18:32:52 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2600938706-c1a07",
+  "appId": "1:843356008542:web:a18414a18923f44e7984cf",
+  "apiKey": "AIzaSyDAyds_Gk9RmMUEmxc6mrQI1h9rr1nP4pw",
+  "authDomain": "studio-2600938706-c1a07.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "843356008542"
+};

6149621137ab573d8de3609bfdf66c16d589acd2
Carlos Guillermo Sánchez Cabezas <carlosguisan@gmail.com>
2025-12-28 22:05:16 +0000

    Try fixing this error: `Build Error: Failed to compile. Error source: ./

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config({ path: './.env' });
-}
-
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,

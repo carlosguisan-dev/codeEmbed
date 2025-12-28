10685200971c987945dd4794d4c3086120485b4c
Carlos Guillermo Sánchez Cabezas <carlosguisan@gmail.com>
2025-12-28 19:40:48 +0000

    podemos usar este favicon: https://50594343.fs1.hubspotusercontent-na1.n

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -137,7 +137,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;600;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@400;500;600&display=swap" rel="stylesheet" />
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚀</text></svg>" />
+        <link rel="icon" href="https://50594343.fs1.hubspotusercontent-na1.net/hubfs/50594343/favicon-s.png" type="image/png" />
       </head>
       <body className="font-body antialiased">
         <FirebaseClientProvider>

20ec5deb8c9a87019ea10ae4c7e19d14cec30054
Carlos Guillermo Sánchez Cabezas <carlosguisan@gmail.com>
2025-12-29 00:34:33 +0000

    el favicon sigue siendo el nativo de firebase, puedes reemplazarlo por e

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -136,7 +136,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link rel="icon" href="/logo.svg" type="image/svg+xml" />
       </head>
       <body className={cn("font-body antialiased", workSans.variable, firaCode.variable)}>
         <FirebaseClientProvider>
